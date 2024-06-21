5b2d30cc173cdeffc5ee8b8b0a1a43415bebfc2e
손인천 <142462485+sonincheon@users.noreply.github.com>
2024-06-22 00:35:44 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int solution(int n) {
+        int sum = 0;
+
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum += i; 
+                if (i != n / i) {
+                    sum += n / i;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+}
